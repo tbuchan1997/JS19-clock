690423c410de291c4558ac715ecd5c5fa0b875b1
Tom Buchan <tbuchan1997@gmail.com>
2024-11-01 14:36:06 +0000

    Add JS functionality

---
+++ script.js
@@ -0,0 +1,46 @@
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+const toggleBtn = document.querySelector(".toggle");
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggleBtn.addEventListener("click", (e) => {
+    const html = document.querySelector("html");
+    if (html.classList.contains("dark")) {
+        html.classList.remove("dark");
+        e.target.innerHTML = "Dark Mode";
+    } else {
+        html.classList.add("dark");
+        e.target.innerHtml = "Light Mode";        
+    }
+})
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours();
+    const hoursForClock = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`
+    dateEl.innerHTML = `${days[day]}, <span class="circle">${date}</span> ${months[month]}`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime, 1000);
